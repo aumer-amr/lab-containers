6828aaa0988c87ec10f8dbd7b263cfb042524135
Aumer <aumer@aumer.dev>
2026-08-20 17:04:09 +0200

    chore: remove all existing old containers

--- apps/pokemon/readme.md
+++
@@ -1,1 +0,0 @@
-Just hosting for a friend smile :)
--- apps/satisfactory/src/index.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config();
-import { login } from './discord';
-
-login();
